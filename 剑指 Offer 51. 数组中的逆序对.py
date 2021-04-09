8452f48113042cbf29c9ce80c214a06d8f6ea39a
Hongyu <286027029@qq.com>
2021-04-09 15:52:43 +0800

    剑指offer51

--- 剑指 Offer 51. 数组中的逆序对.py
+++ 剑指 Offer 51. 数组中的逆序对.py
@@ -1,10 +1,12 @@
-def merge_sort(l, r):
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        def merge_sort(l, r):
             if l >= r: return 0
             m = (l + r) // 2
-            res = merge_sort(l, m) + merge_sort(m + 1, r)
+            res = merge_sort(l, m) + merge_sort(m+1, r)
             i, j = l, m + 1
             tmp[l:r + 1] = nums[l:r + 1]
-            for k in range(l, r + 1):
+            for k in range(l, r+1):
                 if i == m + 1:
                     nums[k] = tmp[j]
                     j += 1
@@ -14,8 +16,9 @@ def merge_sort(l, r):
                 else:
                     nums[k] = tmp[j]
                     j += 1
-                    res += m - i + 1 # 统计逆序对
+                    res += m - i + 1
             return res
         
         tmp = [0] * len(nums)
-        return merge_sort(0, len(nums) - 1)
+        
+        return merge_sort(0, len(nums)-1)

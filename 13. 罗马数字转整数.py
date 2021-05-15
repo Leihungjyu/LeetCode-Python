7861c2a733ba6965a23a7231fa7e64ffd3ef992a
Hongyu <286027029@qq.com>
2021-05-15 10:52:35 +0800

    13. 罗马数字转整数

--- 13. 罗马数字转整数.py
+++ 13. 罗马数字转整数.py
@@ -1,8 +1,7 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        ans, cur = 0, len(s) - 1
-        while cur >= 0:
-            ans = ans - table[s[cur]] if cur < len(s) - 1 and table[s[cur]] < table[s[cur+1]] else ans + table[s[cur]]
-            cur -= 1
+        ans = 0
+        for i in range(len(s)):
+            ans = ans - table[s[i]] if i < len(s) - 1 and table[s[i]] < table[s[i+1]] else ans + table[s[i]]
         return ans
